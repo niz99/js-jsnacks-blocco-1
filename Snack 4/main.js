62870fc53374ad86b1b744238072fa26abe33067
Nizar Fergag <fergag99@gmail.com>
2022-03-16 19:36:50 +0100

    correzione esercizio

--- Snack 4/main.js
+++ Snack 4/main.js
@@ -2,16 +2,16 @@ const nomiInvitati = ["Nizar", "Martina", "Valeria", "Giorgia", "Cristian"];
 
 let nome = prompt("Inserisci il tuo nome: ");
 
+let nomePresente = false;
+
 for(i = 0; i < nomiInvitati.length; i++){
     if(nome === nomiInvitati[i]){
-        document.getElementById("container").innerHTML = `<h1>Sei presente nella lista, puoi entrare!</h1>`;
-    } else {
-        document.getElementById("container").innerHTML = `<h1>Non sei presente nella lista, non puoi entrare!</h1>`;
-    }
+        nomePresente = true; 
+    } 
 }
 
-
-
-
-
-//  || nome = nomiInvitati[i]
+if(nomePresente === true){
+    document.getElementById("container").innerHTML = `<h1>Sei presente nella lista, puoi entrare!</h1>`;
+} else {
+        document.getElementById("container").innerHTML = `<h1>Non sei presente nella lista, non puoi entrare!</h1>`;
+    }
