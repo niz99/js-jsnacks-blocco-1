b8425f8c1a26548ccde296664f43b6980639bf50
Nizar Fergag <fergag99@gmail.com>
2022-03-16 11:45:02 +0100

    esercizio terminato

---
+++ Snack 1/main.js
@@ -0,0 +1,10 @@
+let numeroUno = prompt("Inserisci il primo numero: ");
+let numeroDue = prompt("Inserisci il secondo numero: ");
+
+if (numeroUno > numeroDue) {
+    document.getElementById("container").innerHTML = `<h1>Il primo numero è quello più grande: ${numeroUno}</h1>`;
+} else if (numeroUno < numeroDue) {
+    document.getElementById("container").innerHTML = `<h1>Il secondo numero è quello più grande: ${numeroDue}</h1>`;
+} else {
+    document.getElementById("container").innerHTML = `<h1>I due numeri sono uguali</h1>`;
+}
